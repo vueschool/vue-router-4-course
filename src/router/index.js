96f67926e8536e0233712c0072771fdba5f15467
Daniel Kelly <me@danielkelly.io>
2021-05-20 11:35:34 -0500

    5. lazy loading routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
-import Brazil from '@/views/Brazil.vue'
-import Hawaii from '@/views/Hawaii.vue'
-import Jamaica from '@/views/Jamaica.vue'
-import Panama from '@/views/Panama.vue'
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
-  {path: '/about', name: 'About', component: About},
-  {path:'/brazil', name: 'brazil', component: Brazil},
-  {path:'/hawaii', name: 'hawaii', component: Hawaii},
-  {path:'/jamaica', name: 'jamaica', component: Jamaica},
-  {path:'/panama', name: 'panama', component: Panama},
+  {path:'/brazil', name: 'brazil', component: ()=>import('@/views/Brazil.vue')},
+  {path:'/hawaii', name: 'hawaii', component: ()=>import('@/views/Hawaii.vue')},
+  {path:'/jamaica', name: 'jamaica', component: ()=>import('@/views/Jamaica.vue')},
+  {path:'/panama', name: 'panama', component: ()=>import('@/views/Panama.vue')},
 ]
 
 const router = createRouter({
