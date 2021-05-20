40d5ebd6d7a3c26f55ebb72d298b744941de93bb
Daniel Kelly <me@danielkelly.io>
2021-05-20 16:34:34 -0500

    10b. reacting to param changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <TheNavigation/>
   <div class="container">
-    <router-view></router-view>
+    <router-view :key="$route.path"></router-view>
   </div>
 </template>
 
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="destination">
+  <section v-if="destination" class="destination">
     <h1>{{destination.name}}</h1>
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name">
@@ -14,9 +14,11 @@ export default {
     destinationId(){
       return parseInt(this.$route.params.id)
     },
-    destination(){
-      return sourceData.destinations.find(destination => destination.id === this.destinationId)
-    }
-  }
+    destination() {
+      return sourceData.destinations.find(
+        (destination) => destination.id == this.destinationId
+      );
+    },
+  },
 }
 </script>
