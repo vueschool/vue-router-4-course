82c38fd4cecdbbaeec63977aed295d78e3db2744
Daniel Kelly <me@danielkelly.io>
2021-05-20 11:49:02 -0500

    6. navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
+  <TheNavigation/>
   <div class="container">
     <router-view></router-view>
   </div>
 </template>
 
-<script></script>
+<script>
+import TheNavigation from '@/components/TheNavigation.vue'
+export default{
+  components: {TheNavigation}
+}
+</script>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+</template>
