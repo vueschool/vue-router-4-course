7dda27114e43fa381ac723fd0abfde55d61e2fb9
Daniel Kelly <me@danielkelly.io>
2021-05-20 14:25:17 -0500

    8. dynamic routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ const routes = [
   {path:'/hawaii', name: 'hawaii', component: ()=>import('@/views/Hawaii.vue')},
   {path:'/jamaica', name: 'jamaica', component: ()=>import('@/views/Jamaica.vue')},
   {path:'/panama', name: 'panama', component: ()=>import('@/views/Panama.vue')},
+  {path: '/destination/:id', component: ()=>import('@/views/DestinationShow.vue')}
 ]
 
 const router = createRouter({
---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="destination">
+    <h1>{{destination.name}}</h1>
+    <div class="destination-details">
+      <img :src="`/images/${destination.image}`" :alt="destination.name">
+      <p>{{destination.description}}</p>
+    </div>
+  </section>
+</template>
+<script>
+import sourceData from '@/data.json'
+export default {
+  computed:{
+    destinationId(){
+      return parseInt(this.$route.params.id)
+    },
+    destination(){
+      return sourceData.destinations.find(destination => destination.id === this.destinationId)
+    }
+  }
+}
+</script>
