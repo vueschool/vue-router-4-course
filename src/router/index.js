ee2e9cc8a819b5f00d7adbdc95ab5e1d1264818c
Daniel Kelly <me@danielkelly.io>
2021-05-20 15:13:42 -0500

    9. named routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
   {path:'/hawaii', name: 'hawaii', component: ()=>import('@/views/Hawaii.vue')},
   {path:'/jamaica', name: 'jamaica', component: ()=>import('@/views/Jamaica.vue')},
   {path:'/panama', name: 'panama', component: ()=>import('@/views/Panama.vue')},
-  {path: '/destination/:id', component: ()=>import('@/views/DestinationShow.vue')}
+  {path: '/destination/:id', name: 'destination.show', component: ()=>import('@/views/DestinationShow.vue')}
 ]
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <router-link
         v-for="destination in destinations"
         :key="destination.id"
-        :to="destination.slug"
+        :to="{name: 'destination.show', params:{id: destination.id}}"
       >
         <h2>{{ destination.name }}</h2>
         <img :src="`/images/${destination.image}`" :alt="destination.name" />
