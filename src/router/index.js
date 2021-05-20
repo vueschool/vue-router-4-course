e98eb1af40e81731027399ad1a98830f013d8cba
Daniel Kelly <me@danielkelly.io>
2021-05-20 16:57:02 -0500

    11. route props

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,12 @@ import Home from '@/views/Home.vue'
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
-  {path: '/destination/:id/:slug', name: 'destination.show', component: ()=>import('@/views/DestinationShow.vue')}
+  {
+    path: '/destination/:id/:slug', 
+    name: 'destination.show', 
+    component: ()=>import('@/views/DestinationShow.vue'),
+    props: route=> ({id: parseInt(route.params.id)})
+  }
 ]
 
 const router = createRouter({
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -10,13 +10,13 @@
 <script>
 import sourceData from '@/data.json'
 export default {
+  props:{
+    id: {type: Number, required: true}
+  },
   computed:{
-    destinationId(){
-      return parseInt(this.$route.params.id)
-    },
     destination() {
       return sourceData.destinations.find(
-        (destination) => destination.id == this.destinationId
+        (destination) => destination.id === this.id
       );
     },
   },
