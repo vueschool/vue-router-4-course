6129d9aafb488166f8a6efb54c6c87567bd07e61
Daniel Kelly <me@danielkelly.io>
2021-05-21 13:51:36 -0500

    16. 404 not found

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
       }
     ]
   },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: ()=> import('@/views/NotFound.vue')
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Not Found</h1>
+    <p>
+      Oops, we couldn't find that page. Try going
+      <router-link to="/">home</router-link>
+    </p>
+  </div>
+</template>
