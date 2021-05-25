db4ef2f1ba858013f98a157e8a07c9b7ac62b6b6
Daniel Kelly <me@danielkelly.io>
2021-05-25 10:00:24 -0500

    21. router and the composition api

--- src/views/Invoices.vue
+++ src/views/Invoices.vue
@@ -3,3 +3,16 @@
     <h1>Invoices</h1>
   </div>
 </template>
+<script>
+import {onBeforeRouteLeave} from 'vue-router'
+export default {
+  setup(){
+    onBeforeRouteLeave((to, from)=>{
+      const answer = window.confirm(
+        'Are you sure you want to leave? Invoices are super awesome!'
+      )
+      if(!answer) return false
+    })
+  }
+}
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,19 +11,20 @@
   </div>
 </template>
 <script>
+import {ref} from 'vue'
+import {useRouter, useRoute} from 'vue-router'
 export default {
-  data(){
-    return {
-      username: '',
-      password: ''
+  setup(){
+    const username = ref('')
+    const password = ref('')
+    const router = useRouter()
+    const route = useRoute()
+    const login = ()=>{
+      window.user = username.value
+      const redirectPath = route.query.redirect || '/protected'
+      router.push(redirectPath)
     }
-  },
-  methods: {
-    login(){
-      window.user = this.username
-      const redirectPath = this.$route.query.redirect || '/protected'
-      this.$router.push(redirectPath)
-    }
+    return {username, password, login}
   },
 }
 </script>
