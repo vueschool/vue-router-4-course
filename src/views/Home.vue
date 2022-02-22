6bf6d9be5b1bd009754754ac1c7a27c1ccbc8662
Daniel Kelly <me@danielkelly.io>
2022-02-22 10:11:27 -0600

    28. dynamic routing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>All Destinations</h1>
-    <button @click="triggerRouterError">Trigger Router Error</button>
+    <button @click="addDynamicRoute">Add Dynamic Route</button>
+    <router-link to="/dynamic">Visit Dynamic Route</router-link>
     <div class="destinations">
       <router-link
         v-for="destination in destinations"
@@ -24,14 +25,13 @@ export default {
     };
   },
   methods:{
-    async triggerRouterError(){
-      const navigationFailure = await this.$router.push('/')
-      if(isNavigationFailure(navigationFailure, NavigationFailureType.duplicated)){
-        console.log(navigationFailure.to)
-        console.log(navigationFailure.from)
-      }else{
-        // all is well
-      }
+    addDynamicRoute(){
+      this.$router.addRoute({
+        name: "dynamic",
+        path: "/dynamic",
+        component: () => import("@/views/Login.vue")
+      })
+      this.$router.removeRoute("dynamic")
     }
   }
 };
