7217781d67c3569bf156ba6fb3d7a80aeb24e78e
Daniel Kelly <me@danielkelly.io>
2024-02-12 12:05:09 -0600

    chore:update deps

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias:{
-    '@' : path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-  plugins: [vue()]
-})
+  plugins: [vue()],
+});
